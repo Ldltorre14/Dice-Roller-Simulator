3fbde03933a3a7a44c98c577a68ce974bc369b01
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-11-30 22:52:35 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,25 +1,11 @@
-package sample;
- 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+package application;
+	
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class Main{
+	
+	public static void main(String[] args) {
+		App app = new App();
+		
+		app.run(args);
+	}
 }
-sample.fxml
-<?xml vers
